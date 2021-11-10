2eb68725db00b13985321781682a55fd8b917796
karima <saidaniikarima@gmail.com>
2021-11-10 13:21:19 +0100

    feat_karima ajout java doc pour program classes

--- formationbackend/src/main/java/com/simplon/formation/business/services/interfaces/IProgramService.java
+++ formationbackend/src/main/java/com/simplon/formation/business/services/interfaces/IProgramService.java
@@ -3,16 +3,54 @@ package com.simplon.formation.business.services.interfaces;
 import java.util.List;
 
 import com.simplon.formation.presentation.model.ProgramDto;
+/**
+ * 
+ * IPostServices interface for management business methods for programs
 
+ */
 
 
 public interface IProgramService {
-
+/**
+	 * return the list of all programs
+	 * @return allPrograms
+	 */
 
     List<ProgramDto> getAllPrograms();
+    /**
+	 * return the list of programs from a title
+	 * 
+	 * @param title
+	 * @return allPrograms
+	 */
+	 
     List<ProgramDto> findProgramByTitle(String title);
+    /**
+	 *to search for a program from its id
+	 * 
+	 * @param id
+	 * @return programDto
+	 */
     ProgramDto findProgramById(Long id);
+    /**
+	 * Allows you to create a new Program
+	 * 
+	 * @param programDto
+	 * @return id
+	 */
     void createProgram(ProgramDto programDto);
-    void updateProgram(Long id, ProgramDto postDto);
+    /**
+	 * update a program from its id
+	 * 
+	 * @param id
+	 * @param programDto
+	 * @return id
+	 */
+    void updateProgram(Long id, ProgramDto programDto);
+    /**
+	 *delete a program
+	 * 
+	 * @param id
+	 */
     void deleteProgram(Long id);
 }
