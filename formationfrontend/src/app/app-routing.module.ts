ad7df0f0a51e13a03908365742aa9ef6bc6ec003
Wendy Vandenberghe <wendy.vandenberghe@live.fr>
2021-11-10 13:51:29 +0100

    correction of variable names in English

--- formationfrontend/src/app/app-routing.module.ts
+++ formationfrontend/src/app/app-routing.module.ts
@@ -9,12 +9,12 @@ const routes: Routes = [
   
   { path: 'session', component: SessionListComponent },
   { path: 'session', pathMatch: 'full', redirectTo: '/session' },
-  { path: 'formateur', component: FormerListComponent },
-  { path: 'formateur', pathMatch: 'full', redirectTo: '/formateur' },
-  { path: 'apprenant', component: LearnerListComponent },
-  { path: 'apprenant', pathMatch: 'full', redirectTo: '/apprenant' },
-  { path: 'programme', component: ProgramListComponent },
-  { path: 'programme', pathMatch: 'full', redirectTo: '/programme' },
+  { path: 'former', component: FormerListComponent },
+  { path: 'former', pathMatch: 'full', redirectTo: '/former' },
+  { path: 'learner', component: LearnerListComponent },
+  { path: 'learner', pathMatch: 'full', redirectTo: '/learner' },
+  { path: 'program', component: ProgramListComponent },
+  { path: 'program', pathMatch: 'full', redirectTo: '/program' },
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
   { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
 ];
