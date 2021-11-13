f21cafc55de3d756cdc69115bb53de9b53b65d44
wendy <wendy.vandenberghe@live.fr>
2021-11-13 23:49:04 +0100

    Update program-form.component.ts

--- formationfrontend/src/app/components/program/program-form/program-form.component.ts
+++ formationfrontend/src/app/components/program/program-form/program-form.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-program-form',
   templateUrl: './program-form.component.html',
-  styleUrls: ['./program-form.component.css']
+  styleUrls: ['./program-form.component.css'],
 })
 export class ProgramFormComponent implements OnInit {
+  validateForm!: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  submitForm(): void {
+    console.log('submit', this.validateForm);
+    if (this.validateForm.valid) {
+      console.log('form valid');
+    } else {
+      Object.values(this.validateForm.controls).forEach((control) => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
   }
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      surname: [null, [Validators.required]],
+      name: [null, [Validators.required]],
+    });
+  }
 }
