8bc9383b6afd24e3252262c4b46408a5a9afa21f
wendy <wendy.vandenberghe@live.fr>
2021-11-13 23:48:56 +0100

    Update learner-form.component.ts

--- formationfrontend/src/app/components/learner/learner-form/learner-form.component.ts
+++ formationfrontend/src/app/components/learner/learner-form/learner-form.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-learner-form',
   templateUrl: './learner-form.component.html',
-  styleUrls: ['./learner-form.component.css']
+  styleUrls: ['./learner-form.component.css'],
 })
 export class LearnerFormComponent implements OnInit {
+  validateForm!: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  submitForm(): void {
+    console.log('submit', this.validateForm);
+    if (this.validateForm.valid) {
+      console.log('form valid');
+    } else {
+      Object.values(this.validateForm.controls).forEach((control) => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
   }
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      surname: [null, [Validators.required]],
+      name: [null, [Validators.required]],
+      sessions: [null, [Validators.required]],
+    });
+  }
 }
