03d7c8d47e9734ab27f284f5b53a0740cadedefc
wendy <wendy.vandenberghe@live.fr>
2021-11-14 16:10:55 +0100

    Update session-form.component.ts

--- formationfrontend/src/app/components/session/session-form/session-form.component.ts
+++ formationfrontend/src/app/components/session/session-form/session-form.component.ts
@@ -11,6 +11,7 @@ export class SessionFormComponent implements OnInit {
 
   submitForm(): void {
     console.log('submit', this.validateForm);
+    console.log(this.validateForm.value);
     if (this.validateForm.valid) {
       console.log('form valid');
     } else {
