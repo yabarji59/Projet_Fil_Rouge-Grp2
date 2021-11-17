3d515b96c468c32b08534d45ec37d4b61451556c
Wendy Vandenberghe <wendy.vandenberghe@live.fr>
2021-11-17 16:07:11 +0100

    add files session detail and program  detail and app route

--- formationfrontend/src/app/components/program/program-detail/program-detail.component.ts
+++ formationfrontend/src/app/components/program/program-detail/program-detail.component.ts
@@ -1,4 +1,8 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Program } from 'src/app/model/Program.model';
+import { ProgramService } from 'src/app/services/Program.service';
 
 @Component({
   selector: 'app-program-detail',
@@ -7,7 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgramDetailComponent implements OnInit {
 
-  constructor() { }
+  paramId: string;
+  program: Program;
+    
+  constructor(
+    private programService: ProgramService,
+    private router: Router,
+    private route: ActivatedRoute) {
+    console.log(this.route.snapshot.paramMap.get('id'));
+    this.paramId = this.route.snapshot.paramMap.get('id');
+    if (this.paramId != null) {
+      this.programService
+        .find(this.paramId).subscribe((res: HttpResponse<Program>) => {
+          this.program = res.body;
+        });
+    }
+   }
 
   ngOnInit(): void {
   }
--- formationfrontend/src/app/components/session/session-detail/session-detail.component.ts
+++ formationfrontend/src/app/components/session/session-detail/session-detail.component.ts
@@ -1,4 +1,10 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Learner } from 'src/app/model/Learner.model';
+import { Session } from 'src/app/model/Session.model';
+import { LearnerService } from 'src/app/services/Learner.service';
+import { SessionService } from 'src/app/services/Session.service';
 
 @Component({
   selector: 'app-session-detail',
@@ -6,8 +12,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./session-detail.component.css']
 })
 export class SessionDetailComponent implements OnInit {
+  paramId: string;
+  session: Session;
+  learners: Learner[];
 
-  constructor() { }
+    
+  constructor(
+    private learnerService: LearnerService,
+    private sessionService: SessionService,
+    private router: Router,
+    private route: ActivatedRoute) {
+    console.log(this.route.snapshot.paramMap.get('id'));
+    this.paramId = this.route.snapshot.paramMap.get('id');
+    if (this.paramId != null) {
+      this.sessionService
+        .find(this.paramId).subscribe((res: HttpResponse<Session>) => {
+          this.session = res.body;
+          //this.session.learnersSession = this.getListInSession(this.session);
+        });
+    }
+   }
 
   ngOnInit(): void {
   }
